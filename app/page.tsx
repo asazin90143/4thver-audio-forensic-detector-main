c855f93c9ddbc97cd4389ccac7d770002a1f3e61
KENNETH BRYAN GERABAS ESCALA <69161486+asazin90143@users.noreply.github.com>
2025-08-14 19:07:03 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
+export const revalidate = false;
 
 import type React from "react"
 
